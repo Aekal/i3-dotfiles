fd27b05f8ecbba6c9d2c67af29e4ab4fd5ca806b
Damian Fiałkiewicz <damian.fialkiewicz@snow.dog>
2022-05-28 02:08:37 +0200

    Update dotfiles

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 
 # Terminate already running bar instances
-polybar-msg cmd quit
-# killall -q polybar
+killall -q polybar
+ 
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    notify-send -i display "$m"
-    MONITOR=$m polybar --reload default &
-  done
-else
-  polybar --reload default &
-fi
+PRIMARY=$(xrandr --query | grep " connected" | grep "primary" | cut -d" " -f1)
+OTHERS=$(xrandr --query | grep " connected" | grep -v "primary" | cut -d" " -f1)
+
+# Launch polybar with tray on primary monitor
+MONITOR=$PRIMARY polybar --reload default &
+sleep 1
+
+# Launch polybar on other monitors
+for m in $OTHERS; do
+  MONITOR=$m polybar --reload default &
+done
